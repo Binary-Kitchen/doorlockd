aaf47ab0c7dddacc8b96d14daeb4eb06a7339ee1
Ralf Ramsauer <ralf@ramses-pyramidenbau.de>
2015-09-17 14:50:48 +0200

    Added common file doorcmds.h
    Signed-off-by: Ralf Ramsauer <ralf@ramses-pyramidenbau.de>

---
+++ doorcmds.h
@@ -0,0 +1,14 @@
+#ifndef DOORCMD_H
+#define DOORCMD_H
+
+#define DOOR_CMD_UNLOCK   'u'
+#define DOOR_CMD_LOCK     'l'
+#define DOOR_CMD_SCHNAPER 's'
+#define DOOR_CMD_PING     'p'
+#define DOOR_CMD_STATUS   'i'
+
+#define DOOR_BUTTON_UNLOCK    'U'
+#define DOOR_BUTTON_LOCK      'L'
+#define DOOR_EMERGENCY_UNLOCK 'E'
+
+#endif
