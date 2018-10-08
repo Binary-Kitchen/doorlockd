00f74d69f9e895017dba58d8dd99afcc9897ebdb
Ralf Ramsauer <ralf@binary-kitchen.de>
2018-10-08 22:59:34 +0200

    pydoorlock: Refactor LogicResponse to DoorlockResponse
    Signed-off-by: Ralf Ramsauer <ralf@binary-kitchen.de>

--- pydoorlock/Authenticator.py
+++ pydoorlock/Authenticator.py
@@ -20,34 +20,10 @@ import ldap
 import logging
 
 from enum import Enum
-from random import sample
+
+from .Doorlock import DoorlockResponse
 
 log = logging.getLogger()
-
-# copied from sudo
-eperm_insults = {
-        'Wrong!  You cheating scum!',
-        'And you call yourself a Rocket Scientist!',
-        'No soap, honkie-lips.',
-        'Where did you learn to type?',
-        'Are you on drugs?',
-        'My pet ferret can type better than you!',
-        'You type like i drive.',
-        'Do you think like you type?',
-        'Your mind just hasn\'t been the same since the electro-shock, has it?',
-        'Maybe if you used more than just two fingers...',
-        'BOB says:  You seem to have forgotten your passwd, enter another!',
-        'stty: unknown mode: doofus',
-        'I can\'t hear you -- I\'m using the scrambler.',
-        'The more you drive -- the dumber you get.',
-        'Listen, broccoli brains, I don\'t have time to listen to this trash.',
-        'I\'ve seen penguins that can type better than that.',
-        'Have you considered trying to match wits with a rutabaga?',
-        'You speak an infinite deal of nothing',
-}
-
-def choose_insult():
-    return(sample(eperm_insults, 1)[0])
 
 
 class AuthMethod(Enum):
@@ -61,19 +37,6 @@ class AuthMethod(Enum):
             return 'Local'
         return 'Error'
 
-
-class AuthenticationResult(Enum):
-    Success = 0
-    Perm = 1
-    InternalError = 2
-
-    def __str__(self):
-        if self == AuthenticationResult.Success:
-            return 'Yo, passt!'
-        elif self == AuthenticationResult.Perm:
-            return choose_insult()
-        else:
-            return 'Internal authentication error'
 
 class Authenticator:
     def __init__(self, cfg):
@@ -111,14 +74,14 @@ class Authenticator:
 
     def _try_auth_local(self, user, password):
         if user not in self._local_db:
-            return AuthenticationResult.Perm
+            return DoorlockResponse.Perm
 
         stored_pw = self._local_db[user][0]
         stored_salt = self._local_db[user][1]
         if stored_pw == hashlib.sha256(stored_salt.encode() + password.encode()).hexdigest():
-            return AuthenticationResult.Success
+            return DoorlockResponse.Success
 
-        return AuthenticationResult.Perm
+        return DoorlockResponse.Perm
 
     def _try_auth_ldap(self, user, password):
         log.info('  Trying to LDAP auth (user, password) as user %s', user)
@@ -129,24 +92,24 @@ class Authenticator:
             l.unbind_s()
         except ldap.INVALID_CREDENTIALS:
             log.info('  Invalid credentials')
-            return AuthenticationResult.Perm
+            return DoorlockResponse.Perm
         except ldap.LDAPError as e:
             log.info('  LDAP Error: %s' % e)
-            return AuthenticationResult.InternalError
-        return AuthenticationResult.Success
+            return DoorlockResponse.InternalError
+        return DoorlockResponse.Success
 
     def try_auth(self, credentials):
         if self._simulate:
             log.info('SIMULATION MODE! ACCEPTING ANYTHING!')
-            return AuthenticationResult.Success
+            return DoorlockResponse.Success
 
         method = credentials[0]
         if method not in self._backends:
-            return AuthenticationResult.InternalError
+            return DoorlockResponse.InternalError
 
         if method == AuthMethod.LDAP_USER_PW:
             return self._try_auth_ldap(credentials[1], credentials[2])
         elif method == AuthMethod.LOCAL_USER_DB:
             return self._try_auth_local(credentials[1], credentials[2])
 
-        return AuthenticationResult.InternalError
+        return DoorlockResponse.InternalError
---
+++ pydoorlock/Doorlock.py
@@ -0,0 +1,62 @@
+from enum import Enum
+from random import sample
+
+# copied from sudo
+eperm_insults = {
+        'Wrong!  You cheating scum!',
+        'And you call yourself a Rocket Scientist!',
+        'No soap, honkie-lips.',
+        'Where did you learn to type?',
+        'Are you on drugs?',
+        'My pet ferret can type better than you!',
+        'You type like i drive.',
+        'Do you think like you type?',
+        'Your mind just hasn\'t been the same since the electro-shock, has it?',
+        'Maybe if you used more than just two fingers...',
+        'BOB says:  You seem to have forgotten your passwd, enter another!',
+        'stty: unknown mode: doofus',
+        'I can\'t hear you -- I\'m using the scrambler.',
+        'The more you drive -- the dumber you get.',
+        'Listen, broccoli brains, I don\'t have time to listen to this trash.',
+        'I\'ve seen penguins that can type better than that.',
+        'Have you considered trying to match wits with a rutabaga?',
+        'You speak an infinite deal of nothing',
+}
+
+
+def choose_insult():
+    return(sample(eperm_insults, 1)[0])
+
+
+class DoorlockResponse(Enum):
+    Success = 0
+    Perm = 1
+    AlreadyActive = 2
+    # don't break old apps, value 3 is reserved now
+    RESERVED = 3
+    Inval = 4
+
+    EmergencyUnlock = 10,
+    ButtonLock = 11,
+    ButtonUnlock = 12,
+    ButtonPresent = 13,
+
+    def __str__(self):
+        if self == DoorlockResponse.Success:
+            return 'Yo, passt.'
+        elif self == DoorlockResponse.Perm:
+            return choose_insult()
+        elif self == DoorlockResponse.AlreadyActive:
+            return 'Zustand bereits aktiv'
+        elif self == DoorlockResponse.Inval:
+            return 'Das was du willst geht nicht.'
+        elif self == DoorlockResponse.EmergencyUnlock:
+            return '!!! Emergency Unlock !!!'
+        elif self == DoorlockResponse.ButtonLock:
+            return 'Closed by button'
+        elif self == DoorlockResponse.ButtonUnlock:
+            return 'Opened by button'
+        elif self == DoorlockResponse.ButtonPresent:
+            return 'Present by button'
+
+        return 'Error'
