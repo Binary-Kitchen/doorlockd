5419d4587ab4972a345618978eb93ed7394e6598
Ralf Ramsauer <ralf@binary-kitchen.de>
2021-11-22 21:57:32 +0100

    setup.py: remove pending newline
    Signed-off-by: Ralf Ramsauer <ralf@binary-kitchen.de>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 # Author:
 #  Ralf Ramsauer <ralf@binary-kitchen.de>
 #
-# Copyright (c) Binary Kitchen, 2018
+# Copyright (c) Binary Kitchen, 2018-2021
 #
 # This work is licensed under the terms of the GNU GPL, version 2.  See
 # the COPYING file in the top-level directory.
@@ -13,7 +13,7 @@
 from setuptools import setup, find_packages
 
 with open("VERSION") as version_file:
-    version = version_file.read().lstrip("v")
+    version = version_file.read().lstrip("v").rstrip()
 
 setup(name="pydoorlock", version=version,
       description="A Python interface for doorlock",
