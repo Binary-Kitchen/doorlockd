4aac66d8e0019736e083140fac797cd3d1825122
Thomas Schmid <tom@lfence.de>
2022-11-17 18:48:16 +0100

    Doorlock Backends: Add license header
    Signed-off-by: Thomas Schmid <tom@lfence.de>

--- pydoorlock/AvrDoorlock.py
+++ pydoorlock/AvrDoorlock.py
@@ -1,3 +1,21 @@
+"""
+Doorlockd -- Binary Kitchen's smart door opener
+
+Copyright (c) Binary Kitchen e.V., 2022
+
+Author:
+  Ralf Ramsauer <ralf@binary-kitchen.de>
+  Thomas Schmid <tom@binary-kitchen.de>
+
+This work is licensed under the terms of the GNU GPL, version 2.  See
+the LICENSE file in the top-level directory.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+details.
+"""
+
 import logging
 import threading
 from time import sleep
--- pydoorlock/DoorlockBackend.py
+++ pydoorlock/DoorlockBackend.py
@@ -1,3 +1,20 @@
+"""
+Doorlockd -- Binary Kitchen's smart door opener
+
+Copyright (c) Binary Kitchen e.V., 2022
+
+Author:
+  Thomas Schmid <tom@binary-kitchen.de>
+
+This work is licensed under the terms of the GNU GPL, version 2.  See
+the LICENSE file in the top-level directory.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+details.
+"""
+
 from abc import ABC, abstractmethod
 
 
